bf1a497fc37a1e5572788eeeecb87cfc6d99976d
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-13 20:23:25 +0000

    Initiated the database context class.

---
+++ FDModel/FrameDataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FDModel
+{
+	public class FrameDataContext : DbContext
+	{
+		public static FrameDataContext Instance { get; } = new FrameDataContext();
+
+		public DbSet<Character> Characters { get; set; }
+		public DbSet<Move> Moves { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			if (!options.IsConfigured)
+			{
+				options.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = SFVFrameData;");
+			}
+		}
+	}
+}
