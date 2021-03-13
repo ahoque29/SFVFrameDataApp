b863d93273cdb3fff7aeb352d49f916273454b8a
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-13 20:08:08 +0000

    Changed the name of some properties.

--- FDModel/Character.cs
+++ FDModel/Character.cs
@@ -8,20 +8,24 @@ namespace FDModel
 {
 	public partial class Character
 	{
+		// Foreign keys from Character
 		public Character()
 		{
 			Moves = new HashSet<Move>();
 		}
 
+		// Primary key
 		public int CharacterId { get; set; }
-		public string Name { get; set; }
+
+		public string CharacterName { get; set; }
 		public string Quote { get; set; }
 		public int Health { get; set; }
 		public int Stun { get; set; }
 		public int ForwardDash { get; set; }
 		public int BackDash { get; set; }
-		public string Picture { get; set; }
+		public Uri CharacterPicture { get; set; }
 
+		// Foreign keys from Character
 		public virtual ICollection<Move> Moves { get; set; }
 	}
 }
