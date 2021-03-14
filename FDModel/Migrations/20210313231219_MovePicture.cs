be46426ce5448ec7ffe5b8e0f32f3511c7f63479
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-14 19:14:31 +0000

    Added a new punishment tab and resized the pictures.

--- FDModel/Migrations/20210313231219_MovePicture.cs
+++ FDModel/Migrations/20210313231219_MovePicture.cs
@@ -2,22 +2,22 @@
 
 namespace FDModel.Migrations
 {
-    public partial class MovePicture : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "CharacterPicture",
-                table: "Moves",
-                newName: "MovePicture");
-        }
+	public partial class MovePicture : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "CharacterPicture",
+				table: "Moves",
+				newName: "MovePicture");
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "MovePicture",
-                table: "Moves",
-                newName: "CharacterPicture");
-        }
-    }
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "MovePicture",
+				table: "Moves",
+				newName: "CharacterPicture");
+		}
+	}
 }
--- FDWPF/MainWindow.xaml.cs
+++ FDWPF/MainWindow.xaml.cs
@@ -10,16 +10,19 @@ namespace FDWPF
 	{
 		private CharacterManager _characterManager = new CharacterManager();
 		private MoveManager _moveManager = new MoveManager();
+		private PunishmentManager _punishmentManager = new PunishmentManager();
 
 		public MainWindow()
 		{
 			InitializeComponent();
-			PopulateSelectCharacterComboBox();
+			PopulateSelectCharacterComboBoxes();
 		}
 
-		private void PopulateSelectCharacterComboBox()
+		private void PopulateSelectCharacterComboBoxes()
 		{
-			SelectCharacterComboBox.ItemsSource = _characterManager.RetrieveAllCharacters();
+			SelectCharacterComboBox.ItemsSource =
+				OpponentCharacterComboBox.ItemsSource =
+				YourCharacterComboBox.ItemsSource = _characterManager.RetrieveAllCharacters();
 		}
 
 		private void SelectCharacterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -68,5 +71,65 @@ namespace FDWPF
 				DamageTextBox.Text =
 				StunDamageTextBox.Text = null;
 		}
+
+		private void OpponentCharacterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			DepopulateMovesYouCanPunishWithComboBoxAndPictures();
+			MoveYouCanPunishWithPicture.Source = null;
+
+			_characterManager.SetSelectedCharacter(OpponentCharacterComboBox.SelectedItem);
+			TheirMoveComboBox.ItemsSource = _moveManager.RetrieveACharactersMoveList(_characterManager.SelectedCharacter.CharacterId);
+		}
+
+		private void PopulateMovesYouCanPunishWithComboBoxAndPictures()
+		{
+			if (TheirMoveComboBox.SelectedItem != null && YourCharacterComboBox.SelectedItem != null)
+			{
+				MoveYouCanPunishWithPicture.Source = null;
+
+				_moveManager.SetSelectedMove(TheirMoveComboBox.SelectedItem);
+				_characterManager.SetSelectedCharacter(YourCharacterComboBox.SelectedItem);
+
+				MovesYouCanPunishWithComboBox.ItemsSource = _punishmentManager.ListOfMovesThatCanPunishOnBlock(_characterManager.SelectedCharacter.CharacterId,
+					_moveManager.SelectedMove.OnBlock);
+
+				var resourceUriForTheirMove = new Uri(_moveManager.SelectedMove.MovePicture);
+				TheirMovePicture.Source = new BitmapImage(resourceUriForTheirMove);
+
+				if (MovesYouCanPunishWithComboBox.Items.Count == 0)
+				{
+					var resourceUriSafeOnBlock = new Uri(_punishmentManager.SafeOnBlockPicture);
+					MoveYouCanPunishWithPicture.Source = new BitmapImage(resourceUriSafeOnBlock);
+				}
+			}
+		}
+
+		private void MovesYouCanPunishWithComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (MovesYouCanPunishWithComboBox.SelectedItem != null)
+			{
+				_moveManager.SetSelectedMove(MovesYouCanPunishWithComboBox.SelectedItem);
+				var resourceUriPunish = new Uri(_moveManager.SelectedMove.MovePicture);
+				MoveYouCanPunishWithPicture.Source = new BitmapImage(resourceUriPunish);
+			}
+		}
+
+
+		private void YourCharacterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			PopulateMovesYouCanPunishWithComboBoxAndPictures();
+		}
+
+		private void TheirMoveComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			PopulateMovesYouCanPunishWithComboBoxAndPictures();
+		}
+
+		private void DepopulateMovesYouCanPunishWithComboBoxAndPictures()
+		{
+			MovesYouCanPunishWithComboBox.ItemsSource = null;
+			TheirMovePicture.Source = null;
+		}
+
 	}
 }
