d5e6a11a2c5f7ee989c2c9e27f3aeb15cc5bfc00
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-14 19:05:06 +0000

    added a punishment manager class

---
+++ FDManager/PunishmentManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FDModel;
+
+namespace FDManager
+{
+	public class PunishmentManager
+	{
+		MoveManager _moveManager = new MoveManager();
+
+		public string SafeOnBlockPicture { get; set; }
+
+		public List<Move> ListOfMovesThatCanPunishOnBlock(int yourCharacterId, int opponentMoveOnBlock)
+		{
+			var yourMoves = _moveManager.RetrieveACharactersMoveList(yourCharacterId);
+			var movesThatCanPunishOnBlock = yourMoves.Where(y => y.StartUp <= -opponentMoveOnBlock).ToList();
+
+			if (!movesThatCanPunishOnBlock.Any())
+			{
+				SafeOnBlockPicture = @"C:\Users\gaffi\source\repos\SFVFrameDataApp\FDModel\Pictures\MovePictures\safeonblock.png";
+			}
+
+			return movesThatCanPunishOnBlock;
+		}
+	}
+}
